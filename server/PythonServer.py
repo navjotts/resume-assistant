99a266edfca08ffade5e291021d4d0b02a3d825e
Navjot Singh <navjotts@gmail.com>
2018-08-14 15:44:12 -0700

    introduce a sepearate sentence splitter

--- server/PythonServer.py
+++ server/PythonServer.py
@@ -1,9 +1,19 @@
 import zerorpc
+
 from py.Spacy import Spacy
 
 class PythonServer(object):
     def sentences(self, text):
-        return Spacy.sentences(self, text)
+        return Spacy.sentences(self, text, True)
+
+    def sentences_from_file_line(self, filepath):
+        sents = []
+        with open(filepath, encoding = 'utf8') as f:
+            for line in f:
+                sentence = Spacy.tokenize(self, line, False)
+                sents.append(sentence)
+        return sents
+
 
 try:
     s = zerorpc.Server(PythonServer())
--- server/py/Spacy.py
+++ server/py/Spacy.py
@@ -6,7 +6,7 @@ nlp.max_length = 2000000 # todo check
 import time
 
 class Spacy(object):
-    def sentences(self, text):
+    def sentences(self, text, drop_stop_words):
         sents = []
         sentence = []
         for t in nlp(str(text)):
@@ -14,7 +14,15 @@ class Spacy(object):
                 if len(sentence) > 0:
                     sents.append(sentence)
                 sentence = []
-            if not t.is_space and not t.is_stop and not t.is_punct:
+            if not t.is_space and not (drop_stop_words and t.is_stop) and not t.is_punct:
                 sentence.append(t.text)
 
         return sents
+
+    def tokenize(self, text, drop_stop_words):
+        tokens = []
+        for t in nlp(str(text)):
+            if not t.is_space and not (drop_stop_words and t.is_stop) and not t.is_punct:
+                tokens.append(t.text)
+
+        return tokens
