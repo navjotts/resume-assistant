68fbcdbf227ac9c696bcd630e8e5a43715a0aebc
Navjot Singh <navjotts@gmail.com>
2018-08-15 01:19:38 -0700

    add anonymization

--- server/py/Spacy.py
+++ server/py/Spacy.py
@@ -1,11 +1,22 @@
+import re
+
 import spacy
 import en_core_web_sm
 nlp = en_core_web_sm.load()
-nlp.max_length = 2000000 # todo check
+nlp.max_length = 2000000 # todo: check this limit
 
 import time
 
 class Spacy(object):
+    def anonymize(self, t):
+        if t.like_email: # todo: bring in Human Names as well
+            return 'X'*len(t.text)
+        return t.text
+
+    def is_phone_number(self, text):
+        phone_regex = re.compile('(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})') # todo: cover international
+        return phone_regex.match(text)
+
     def sentences(self, text, drop_stop_words):
         sents = []
         sentence = []
@@ -14,15 +25,18 @@ class Spacy(object):
                 if len(sentence) > 0:
                     sents.append(sentence)
                 sentence = []
-            if not t.is_space and not (drop_stop_words and t.is_stop) and not t.is_punct:
-                sentence.append(t.text)
+            if not t.is_space and not (drop_stop_words and t.is_stop):
+                sentence.append(Spacy.anonymize(self, t))
 
         return sents
 
     def tokenize(self, text, drop_stop_words):
         tokens = []
+        is_phone_number = Spacy.is_phone_number(self, text)
         for t in nlp(str(text)):
-            if not t.is_space and not (drop_stop_words and t.is_stop) and not t.is_punct:
-                tokens.append(t.text)
+            if not t.is_space and is_phone_number:
+                tokens.append(t.text if t.is_punct else 'X'*len(t.text))
+            elif not t.is_space and not (drop_stop_words and t.is_stop):
+                tokens.append(Spacy.anonymize(self, t))
 
         return tokens
