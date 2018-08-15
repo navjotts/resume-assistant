a3dd21c18027063826412acbd15e3fa3b441fbc3
Navjot Singh <navjotts@gmail.com>
2018-08-15 14:31:49 -0700

    fetch resumes by default on the training page

--- client/public/js/Client.js
+++ client/public/js/Client.js
@@ -51,3 +51,10 @@ function uploadResume() {
 function uploadJob() {
     alert('uploadJob() not implemented yet!');
 }
+
+// TODO proper way to do this kind of stuff
+$(document).ready(function () {
+  if (document.title === 'Training') {
+      fetchResumes();
+  }
+});
