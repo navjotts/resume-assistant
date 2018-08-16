46632d55a9bd24ce60bf1e1a585fb39e3c3d8e89
Navjot Singh <navjotts@gmail.com>
2018-08-16 16:44:51 -0700

    consolidate duplicate code

--- server/MLHelper.js
+++ server/MLHelper.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const PythonConnector = require('./PythonConnector.js');
 
-async function train(name) {
+function collectData(parent, name) {
     var samples = [];
     var labels = [];
-    var dbDir = path.join(__dirname, 'data', 'DB', name)
+    var dbDir = path.join(__dirname, 'data', parent, name)
     var files = fs.readdirSync(dbDir);
     for (var i = 0; i < files.length; i++) {
         var fileName = files[i];
@@ -19,10 +19,16 @@ async function train(name) {
         }
     }
 
-    console.log(`Starting training on data size of (samples, labels): (${samples.length}, ${labels.length})`);
+    return {samples, labels};
+}
+
+async function train(name) {
+    var data = collectData('DB', name);
+    console.log(`Starting training on data size of (samples, labels): (${data.samples.length}, ${data.labels.length})`);
+
     try {
         var modelPath = path.join(__dirname, 'data', 'models', name);
-        var result = await PythonConnector.invoke('train_sentence_classifier', name, modelPath, samples, labels);
+        var result = await PythonConnector.invoke('train_sentence_classifier', name, modelPath, data.samples, data.labels);
         console.log(`Training result of ${name}: Precision=${result.precision}, Recall=${result.precision}`);
     }
     catch (e) {
@@ -31,28 +37,15 @@ async function train(name) {
 }
 
 async function test(name) {
-    var samples = [];
-    var labels = [];
-    var dbDir = path.join(__dirname, 'data', 'testDB', name)
-    var files = fs.readdirSync(dbDir);
-    for (var i = 0; i < files.length; i++) {
-        var fileName = files[i];
-        if (fileName.split('.').pop() === 'json') {
-            var docData = JSON.parse(fs.readFileSync(path.join(dbDir, fileName)));
-            docData.content.forEach(each => {
-                samples.push(each.sentence.join(' '));
-                labels.push(each.label);
-            });
-        }
-    }
+    var data = collectData('testDB', name);
+    console.log(`Starting testing on data size of (samples, labels): (${data.samples.length}, ${data.labels.length})`);
 
-    console.log(`Starting testing on data size of (samples, labels): (${samples.length}, ${labels.length})`);
     try {
         var modelPath = path.join(__dirname, 'data', 'models', name);
-        await PythonConnector.invoke('test_sentence_classifier', name, modelPath, samples, labels);
+        await PythonConnector.invoke('test_sentence_classifier', name, modelPath, data.samples, data.labels);
     }
     catch (e) {
-        console.log('Error in train:', e);
+        console.log('Error in test:', e);
     }
 }
 
