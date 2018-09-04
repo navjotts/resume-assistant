2e2191cb14e54201e3990cd0400af436fab1b665
astrebeck <alexstrebeck@gmail.com>
2018-09-04 11:57:51 -0600

    added baseline logistic regression model

---
+++ server/py_files/ML_models/Log_reg/Logistic_reg.py
@@ -0,0 +1,8 @@
+#import relevent models
+
+
+#Overall class for the log reg model
+
+## create the acutal model, most likely form sklearn
+
+## methods of that object train, predict, save, load, productionize
