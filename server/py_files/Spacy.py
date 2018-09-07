6256070d97d192b71132a4d0834cb4e4cf835946
Antonio Hernandez <antonio@antoniohernandez.me>
2018-09-07 13:06:49 -0500

    Added anonymize_name function

--- server/py_files/Spacy.py
+++ server/py_files/Spacy.py
@@ -1,8 +1,8 @@
 import re
 
 import spacy
-import en_core_web_sm
-nlp = en_core_web_sm.load()
+import en_core_web_lg
+nlp = en_core_web_lg.load()
 nlp.max_length = 2000000 # todo check this limit
 
 class Spacy(object):
@@ -10,6 +10,16 @@ class Spacy(object):
         if t.like_email: # todo bring in Human Names as well
             return 'X'*len(t.text)
         return t.text
+    
+    def anonymize_name(self, text):
+        doc = nlp(text)
+        spans = list(doc.ents)
+
+        for span in spans:
+            span.merge()
+        
+        for person in filter(lambda w: w.ent_type_ = 'PERSON', doc):
+            return ('XXXXXXXXXX')
 
     # todo: case to check Cc @Antonio - how does the search function behaves if there are more than 1 matches (what happens if there are 2 phone numbers in 1 sentence)
     def anonymize_phone_number(self, text):
@@ -37,9 +47,14 @@ class Spacy(object):
 
     def tokenize(self, text, drop_stop_words):
         tokens = []
+        text = Spacy.anonymize_name(self, text)
         text = Spacy.anonymize_phone_number(self, text)
         for t in nlp(str(text)):
             if not t.is_space and not (drop_stop_words and t.is_stop):
                 tokens.append(Spacy.anonymize_token(self, t))
 
         return tokens
+
+#findall(pattern, string, flags=0)
+#Return all non-overlapping matches of pattern in string, as a list of strings. The string is scanned left-to-right, and matches are returned in the order found. If one or more groups are present in the pattern, return a list of groups; this will be a list of tuples if the pattern has more than one group. Empty matches are included in the result.
+#
