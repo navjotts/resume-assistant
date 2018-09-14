3a7079924ff898532dbb746e04f10ab3770bccb2
Antonio Hernandez <antonio@antoniohernandez.me>
2018-09-14 09:33:28 -0500

    most changes made were to generate plots

--- server/py_files/recreate_data.py
+++ server/py_files/recreate_data.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 # This script assumes that it will be used in a *nix environment
 import os
+import glob
 import subprocess
 
 # Delete especified file types 
@@ -15,9 +16,19 @@ def delete_data():
     # Delete txt resumes
     delete_files('server/data/resumes-txt/*.txt')
 
+def generate_db():
     # I have not found a proper way to call the GenerateDB.js function from python.
-    subprocess.call("node server/GenerateDB.js", shell=True)
+    subprocess.call("/usr/bin/node server/GenerateDB.js", shell=True)  # I read that shell=True is not safe.
+
+def concatenate_txt_resumes():
+    # This will delete the old all_resumes.txt file and create a new one.
+    os.system("find server/data/resumes-txt -type f -name '*.txt' -exec cat {} +  > all_resumes.txt")
+
 
 
 # Delete the data
 delete_data()
+# Generate the .json and .txt resumes
+generate_db()
+# Concatenate all .txt resumes into a single file
+concatenate_txt_resumes()
