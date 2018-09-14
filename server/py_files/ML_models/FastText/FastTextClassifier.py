632aea7dd6b92379305f2a9308cc4b156c6d93c2
Navjot Singh <navjotts@gmail.com>
2018-09-13 18:05:05 -0700

    leftover cleanups in FastTextClassifier

--- server/py_files/ML_models/FastText/FastTextClassifier.py
+++ server/py_files/ML_models/FastText/FastTextClassifier.py
@@ -6,21 +6,20 @@ import numpy as np
 from py_files.AccuracyAnalysis import AccuracyAnalysis
 
 class FastTextClassifier(object):
-    models = {}
-
     def __init__(self, name):
         self.name = name
         self.path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'weights', 'resumes')
+        self.models = {}
 
     def train(self, samples, labels):
         model = None
         fd, labelPath = tempfile.mkstemp()
 
         try:
-            FastTextClassifier.generate_data_file(self, labelPath, samples, labels)
+            self.generate_data_file(labelPath, samples, labels)
             print('Training started ...')
             model = fastText.train_supervised(labelPath, epoch=30, wordNgrams=2)
-            FastTextClassifier.models[self.name] = model
+            self.models[self.name] = model
             print('Training ended ...')
 
             # print("Quantizing ...")
@@ -40,10 +39,10 @@ class FastTextClassifier(object):
         return result
 
     def test(self, samples, labels):
-        if self.name not in FastTextClassifier.models:
-            FastTextClassifier.load(self)
+        if self.name not in self.models:
+            self.load()
 
-        model = FastTextClassifier.models[self.name]
+        model = self.models[self.name]
 
         pred, prob = model.predict(list(samples))
         labels_pred = [each[0][len('__label__'):] for each in pred]
@@ -70,10 +69,10 @@ class FastTextClassifier(object):
         if test:
             return self.test(samples, labels)
 
-        if self.name not in FastTextClassifier.models:
-            FastTextClassifier.load(self)
+        if self.name not in self.models:
+            self.load()
 
-        model = FastTextClassifier.models[self.name]
+        model = self.models[self.name]
 
         pred, prob = model.predict(list(samples))
         labels_pred = [each[0][len('__label__'):] for each in pred]
@@ -83,13 +82,13 @@ class FastTextClassifier(object):
 
     def load(self):
         model = None
-        if self.name not in FastTextClassifier.models:
+        if self.name not in self.models:
             try:
                 model = fastText.load_model(self.path + '.bin')
             except:
                 print('Unable to locate model', self.name)
 
-        FastTextClassifier.models[self.name] = model
+        self.models[self.name] = model
 
     def generate_data_file(self, file, samples, labels):
         text = ''
