7dfdeb744524bca00d3d09590340e4b7da0dd9f0
Navjot Singh <navjotts@gmail.com>
2018-09-25 03:23:02 -0700

    separate the embeddings_layer creation also into a separate function - so that we can control whether to freeze the embeddings or train them further

--- server/py_files/models/CNNClassifier/CNNClassifier.py
+++ server/py_files/models/CNNClassifier/CNNClassifier.py
@@ -22,23 +22,8 @@ class CNNClassifier(KerasSentenceClassifier):
     def train(self, samples, labels):
         features = self.choose_features(samples, True)
 
-        embedding_size = 100
-        embedding_layer = None
-        if self.feature_type == 'word-embeddings':
-            x_train = pad_sequences(features, maxlen=self.max_len, padding='post')
-            pretrained_embeddings = Embeddings(self.name, embedding_size).vectors()
-            vocab_size = pretrained_embeddings.shape[0]
-            embedding_layer = Embedding(input_dim=vocab_size, output_dim=embedding_size,
-                                input_length=self.max_len, trainable=True, weights=[pretrained_embeddings])
-        elif self.feature_type in ['tf-idf', 'bow']:
-            x_train = features
-            vocab_size = x_train.shape[1]
-            embedding_layer = Embedding(input_dim=vocab_size, output_dim=embedding_size,
-                                input_length=vocab_size, trainable=True)
-        else:
-            raise Exception('Please select a valid feature')
-
-        self.model = self.vanilla_CNN(embedding_layer)
+        # self.model, x_train = self.experimental_CNN(features=features, trainable_embeddings=True) # uncomment to use this
+        self.model, x_train = self.vanilla_CNN(features=features, trainable_embeddings=True)
 
         numeric_labels = SentenceLabelEncoder().encode_numerical(labels)
         class_weights = class_weight.compute_class_weight('balanced', np.unique(numeric_labels), numeric_labels)
@@ -79,7 +64,26 @@ class CNNClassifier(KerasSentenceClassifier):
         # todo
         return super().classify(samples)
 
-    def experimental_CNN(self, embedding_layer):
+    def embedding_layer(self, embedding_size, trainable, features):
+        embedding_layer = None
+        if self.feature_type == 'word-embeddings':
+            x_train = pad_sequences(features, maxlen=self.max_len, padding='post')
+            pretrained_embeddings = Embeddings(self.name, embedding_size).vectors()
+            vocab_size = pretrained_embeddings.shape[0]
+            embedding_layer = Embedding(input_dim=vocab_size, output_dim=embedding_size,
+                                input_length=self.max_len, trainable=trainable, weights=[pretrained_embeddings])
+        elif self.feature_type in ['tf-idf', 'bow']:
+            x_train = features
+            vocab_size = x_train.shape[1]
+            embedding_layer = Embedding(input_dim=vocab_size, output_dim=embedding_size,
+                                input_length=vocab_size, trainable=trainable)
+        else:
+            raise Exception('Please select a valid feature')
+
+        return embedding_layer, x_train
+
+    def experimental_CNN(self, features, trainable_embeddings):
+        embedding_layer, x_train = self.embedding_layer(embedding_size=100, trainable=trainable_embeddings, features=features)
         model = Sequential()
         model.add(embedding_layer)
         model.add(Conv1D(filters=256, kernel_size=5, strides=4, padding='valid', activation='relu', data_format='channels_first'))
@@ -96,9 +100,10 @@ class CNNClassifier(KerasSentenceClassifier):
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
         print(model.summary())
 
-        return model
+        return model, x_train
 
-    def vanilla_CNN(self, embedding_layer):
+    def vanilla_CNN(self, features, trainable_embeddings):
+        embedding_layer, x_train = self.embedding_layer(embedding_size=100, trainable=trainable_embeddings, features=features)
         model = Sequential()
         model.add(embedding_layer)
         model.add(Conv1D(filters=256, kernel_size=5, padding='same', activation='relu'))
@@ -109,4 +114,4 @@ class CNNClassifier(KerasSentenceClassifier):
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
         print(model.summary())
 
-        return model
+        return model, x_train
