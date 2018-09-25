bedf7bcc63610004d0e89856ade914011e9cab52
Navjot Singh <navjotts@gmail.com>
2018-09-25 06:25:45 -0700

    checkin updated model for LSTM - the old checkedin model seems to have been trained on a different shape of data

--- server/py_files/models/LSTM/LSTMClassifier.py
+++ server/py_files/models/LSTM/LSTMClassifier.py
@@ -49,7 +49,7 @@ class LSTMClassifier(KerasSentenceClassifier):
         class_weights = class_weight.compute_class_weight('balanced', np.unique(numeric_labels), numeric_labels)
         y_train = SentenceLabelEncoder().encode_categorical(labels)
 
-        self.model.fit(x_train, y_train, validation_split=0.2, epochs=5, batch_size=32,
+        self.model.fit(x_train, y_train, validation_split=0.2, epochs=10, batch_size=32,
                         verbose=2, shuffle=True, class_weight=class_weights)
         loss, accuracy = self.model.evaluate(x_train, y_train)
         print('loss, accuracy:', loss, accuracy)
